8858a2d04c99585a1b2a9cfde92669630ab59d25
renves <renan_alves1921@hotmail.com>
2021-04-02 20:24:32 -0300

    test

--- cronjob.py
+++ cronjob.py
@@ -7,7 +7,7 @@ from main import ScrapSpider
 
 process = CrawlerProcess()
 scheduler = TwistedScheduler()
-scheduler.add_job(process.crawl, 'cron', args=[ScrapSpider], hour=20, minute=25)
+scheduler.add_job(process.crawl, 'cron', args=[ScrapSpider], hour=20, minute=30)
 scheduler.add_job(process.crawl, 'interval', args=[ScrapSpider], seconds=60)
 scheduler.start()
 process.start(False)
