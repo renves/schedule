0bb4ce572f2e41ad7aaf13e8c695ba17d3594c79
renves <renan_alves1921@hotmail.com>
2021-04-02 21:52:46 -0300

    final test

--- cronjob.py
+++ cronjob.py
@@ -8,14 +8,15 @@ process = CrawlerProcess()
 scheduler = TwistedScheduler()
 
 scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUK], hour=10, timezone='UTC')
-scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUK], hour=10)
+scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUK], hour=13, timezone='UTC')
 scheduler.add_job(process.crawl, 'cron', args=[
-                  ScrapSpiderUK], hour=12, minute=30)
+                  ScrapSpiderUK], hour=12, minute=30, timezone='UTC')
 
-scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUS], hour=20, minute=52,timezone='US/Eastern')
-scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUS], hour=14)
+scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUS], hour=20, minute=54, timezone='US/Eastern')
+scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUS], hour=20, minute=55, timezone='US/Eastern')
+scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUS], hour=14, timezone='US/Eastern')
 scheduler.add_job(process.crawl, 'cron', args=[
-                  ScrapSpiderUS], hour=16, minute=30)
+                  ScrapSpiderUS], hour=16, minute=30, timezone='US/Eastern')
 
 scheduler.start()
 process.start(False)
