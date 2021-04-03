4086ce504d18de83d4331292fa73692d162fdb32
renves <renan_alves1921@hotmail.com>
2021-04-02 22:02:05 -0300

    file adjust

--- test.py
+++
@@ -1,9 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from main import ScrapSpider
-import ipdb
-if __name__ == '__main__':
-    process = CrawlerProcess()
-    process.crawl(ScrapSpider)
-    ipdb.set_trace()
-    process.start()
-    ipdb.set_trace()
