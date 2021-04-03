9ac4072ddd90d166c82b3500796b25bc06a85c90
renves <renan_alves1921@hotmail.com>
2021-04-02 21:55:15 -0300

    final test

--- cronjob.py
+++ cronjob.py
@@ -12,8 +12,8 @@ scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUK], hour=13, timezone
 scheduler.add_job(process.crawl, 'cron', args=[
                   ScrapSpiderUK], hour=12, minute=30, timezone='UTC')
 
-scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUS], hour=20, minute=54, timezone='US/Eastern')
-scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUS], hour=20, minute=55, timezone='US/Eastern')
+scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUS], hour=20, minute=57, timezone='US/Eastern')
+scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUS], hour=20, minute=58, timezone='US/Eastern')
 scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUS], hour=14, timezone='US/Eastern')
 scheduler.add_job(process.crawl, 'cron', args=[
                   ScrapSpiderUS], hour=16, minute=30, timezone='US/Eastern')
