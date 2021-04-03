ab049cfaf34b4b5347ea7169e0017660ec444665
renves <renan_alves1921@hotmail.com>
2021-04-02 21:42:05 -0300

    final test

--- cronjob.py
+++ cronjob.py
@@ -7,12 +7,12 @@ from uk import ScrapSpiderUK
 process = CrawlerProcess()
 scheduler = TwistedScheduler()
 
-scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUK], hour=7, timezone='UTC')
+scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUK], hour=10, timezone='UTC')
 scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUK], hour=10)
 scheduler.add_job(process.crawl, 'cron', args=[
                   ScrapSpiderUK], hour=12, minute=30)
 
-scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUS], hour=11)
+scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUS], hour=11, timezone='EDC')
 scheduler.add_job(process.crawl, 'cron', args=[ScrapSpiderUS], hour=14)
 scheduler.add_job(process.crawl, 'cron', args=[
                   ScrapSpiderUS], hour=16, minute=30)
